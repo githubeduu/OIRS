78616d47d8b9b5d1dd43f5905a4217fdbd6d1a51
Eduardo <edo.carvajal@hotmail.com>
2025-05-27 18:56:46 -0400

    chore: se agrega funcionalidad msal

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,29 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { PublicClientApplication } from '@azure/msal-browser';
+import { msalConfig } from './app/msal.config';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './app/auth.interceptor';
+import { routes } from './app/app.routes';
+import { MSAL_INSTANCE, MsalService } from '@azure/msal-angular';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+
+function MSALInstanceFactory() {
+  const instance = new PublicClientApplication(msalConfig);
+  instance.initialize();
+  return instance;
+}
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([authInterceptor])),
+    {
+      provide: MSAL_INSTANCE,
+      useFactory: MSALInstanceFactory,
+    },
+    MsalService,
+  ]
+}).catch(err => console.error(err));
+
